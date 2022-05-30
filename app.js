347a69eec41dabaf00e8d68ceb810684fd6f740e
QamarDeenSoomro <qamersoomro1993@gmail.com>
2022-05-30 16:36:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,343 @@
+var UsersData = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Elvis",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "Roscoeview",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342",
+      },
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "Keebler LLC",
+      catchPhrase: "User-centric fault-tolerant solution",
+      bs: "revolutionize end-to-end systems",
+    },
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Christy",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478",
+      },
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "Considine-Lockman",
+      catchPhrase: "Synchronised bottom-line interface",
+      bs: "e-enable innovative applications",
+    },
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Howemouth",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984",
+      },
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Johns Group",
+      catchPhrase: "Configurable multimedia task-force",
+      bs: "generate enterprise e-tailers",
+    },
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Aliyaview",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677",
+      },
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers",
+    },
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889",
+      },
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "Yost and Sons",
+      catchPhrase: "Switchable contextually-based project",
+      bs: "aggregate real-time technologies",
+    },
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Lebsackbury",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232",
+      },
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "Hoeger LLC",
+      catchPhrase: "Centralized empowering task-force",
+      bs: "target end-to-end models",
+    },
+  },
+];
+var main = document.getElementById("main");
+var UserCont = document.createElement("div");
+UserCont.setAttribute("id", "UsersMainDiv");
+main.appendChild(UserCont);
+// LOOP
+for (var i = 0; i < UsersData.length; i++) {
+  //Users Div
+  var usersDiv = document.createElement("div");
+  UserCont.appendChild(usersDiv);
+  usersDiv.setAttribute("class", "UserDiv");
+  //User ID
+  var UserID = document.createElement("p");
+  var UserIdNumbr = document.createTextNode(UsersData[i].id);
+  UserID.appendChild(UserIdNumbr);
+  UserID.setAttribute("class", "UserID")
+  usersDiv.appendChild(UserID);
+  
+  //Full Name
+  var FullName = document.createElement("p");
+  // var FullNameTxt = document.createTextNode("Name: "+UsersData[i].name);
+  FullName.setAttribute("class","FullName")
+  FullName.appendChild(document.createTextNode(UsersData[i].name));
+  usersDiv.appendChild(FullName);
+  //User Name
+  var UserName = document.createElement("p");
+  var UserNameTxt = document.createTextNode(UsersData[i].username);
+  UserName.appendChild(UserNameTxt);
+  usersDiv.appendChild(UserName);
+  // Email / Phone Div
+  var EmPhDiv = document.createElement("div")
+  usersDiv.appendChild(EmPhDiv);
+  EmPhDiv.setAttribute("class","EmPhDiv")
+  //Email
+  var UEmail = document.createElement("p");
+  var EmailIcon = document.createElement("img")
+  EmailIcon.setAttribute("src","./icons/email.png")
+  EmailIcon.setAttribute("class","icons")
+  UEmail.appendChild(EmailIcon)
+  UEmail.appendChild(document.createTextNode(UsersData[i].email));
+  UEmail.setAttribute("class","UEmail")
+  EmPhDiv.appendChild(UEmail);
+  //Address Div
+  var AddressDiv = document.createElement("div");
+  var AddressDivIcon = document.createElement("img");
+  AddressDivIcon.setAttribute("src","./icons/address.png")
+  AddressDivIcon.setAttribute("class", "icons");
+  var FullAddress = document.createTextNode(UsersData[i].address.suite + ", " + UsersData[i].address.street + ", " + UsersData[i].address.city+" ,"+UsersData[i].address.zipcode );
+  var Addressbr = document.createElement("br");
+  var AddressL2 = document.createTextNode(UsersData[i].address.geo.lat + "," + UsersData[i].address.geo.lng);
+  var AddressPinIcon = document.createElement("img");
+  AddressPinIcon.setAttribute("src","./icons/location.png")
+  AddressPinIcon.setAttribute("class","icons")
+  usersDiv.appendChild(AddressDiv);
+  
+  AddressDiv.setAttribute("class", "AddressDiv");
+  // Address L2 
+  AddressL2Div = document.createElement("div");
+  usersDiv.appendChild(AddressL2Div);
+  AddressDiv.appendChild(AddressDivIcon)
+  AddressDiv.appendChild(FullAddress);
+  AddressDiv.appendChild(Addressbr);
+  AddressL2Div.appendChild(AddressPinIcon);
+  AddressL2Div.appendChild(AddressL2);
+  AddressL2Div.setAttribute("class","AddressDiv")
+  // Phone 
+  var Phone = document.createElement("p");
+  var PhoneIcon = document.createElement("img");
+  PhoneIcon.setAttribute("src","./icons/phone-call.png")
+  PhoneIcon.setAttribute("class","icons")
+  var PhoneTxt = document.createTextNode(UsersData[i].phone);
+  Phone.appendChild(PhoneIcon)
+  Phone.appendChild(PhoneTxt);
+  EmPhDiv.appendChild(Phone);
+  //Website
+  var Website = document.createElement("a");
+  var webURL = "https://" + UsersData[i].website
+  var WebsiteTxt = document.createTextNode(UsersData[i].website);
+  var WebsiteIcon = document.createElement("img")
+  WebsiteIcon.setAttribute("src","./icons/globe.png");
+  WebsiteIcon.setAttribute("class","icons");
+  Website.appendChild(WebsiteIcon)
+  Website.setAttribute("href", webURL);
+  Website.appendChild(WebsiteTxt);
+  AddressL2Div.appendChild(Website);
+  //Company Div
+  CompanyDiv = document.createElement("div");
+  CompanyDiv.setAttribute("class", "companyDiv");
+  usersDiv.appendChild(CompanyDiv);
+  CompanyDivTxt = document.createTextNode("Company:");
+  CompanyDiv.appendChild(CompanyDivTxt);
+  //Comapny DAtA
+  var CompanyName = document.createElement("p");
+  var ComapnyNameTxt = document.createTextNode(UsersData[i].company.name);
+  CompanyName.appendChild(ComapnyNameTxt);
+  CompanyName.setAttribute("class","companyName")
+  CompanyDiv.appendChild(CompanyName);
+  CompanyTitle = document.createElement("p");
+  companyTitleTxt = document.createTextNode(UsersData[i].company.catchPhrase)
+  CompanyTitle.appendChild(companyTitleTxt);
+  CompanyTitle.setAttribute("class","companyTitle")
+  CompanyDiv.appendChild(CompanyTitle);
+  CompanyBS = document.createElement("p");
+  companyBSTxt = document.createTextNode(UsersData[i].company.bs);
+  CompanyBS.appendChild(companyBSTxt);
+  CompanyDiv.appendChild(CompanyBS);
+
+}
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+
+// }
